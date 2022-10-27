ad291dea40bc89fa204edf4f704cfcdc0f488afe
Nikita Novikov <sirpointer@gmail.com>
2022-10-27 15:47:58 +0400

    Designing the interface

--- Project 13 Instafilter/Project 13 Instafilter/Project 13 Instafilter/ViewController.swift
+++ Project 13 Instafilter/Project 13 Instafilter/Project 13 Instafilter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var intensitySlider: UISlider!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
 }
 
