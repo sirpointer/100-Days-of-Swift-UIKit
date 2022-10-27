2c8ccf423e1140de1f1264998356f922223c6ca4
Nikita Novikov <sirpointer@gmail.com>
2022-10-27 19:46:58 +0400

    Setting up

---
+++ Project 14 Whack-a-Penguin/Project 14 Whack-a-Penguin/Project 14 Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project 14 Whack-a-Penguin
+//
+//  Created by Nikita Novikov on 27.10.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
