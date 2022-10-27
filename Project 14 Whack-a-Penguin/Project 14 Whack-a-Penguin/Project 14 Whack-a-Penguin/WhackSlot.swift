6d882c1568a955694ee3792d64329e73e9ec798f
Nikita Novikov <sirpointer@gmail.com>
2022-10-27 20:24:03 +0400

    Getting up and running: SKCropNode

---
+++ Project 14 Whack-a-Penguin/Project 14 Whack-a-Penguin/Project 14 Whack-a-Penguin/WhackSlot.swift
@@ -0,0 +1,32 @@
+//
+//  WhackSlot.swift
+//  Project 14 Whack-a-Penguin
+//
+//  Created by Nikita Novikov on 27.10.2022.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+}
