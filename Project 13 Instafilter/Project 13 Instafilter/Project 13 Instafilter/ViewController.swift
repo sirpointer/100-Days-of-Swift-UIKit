c0d85bcc4bff95eb0b8cc7a40b656e4c82275ac0
Nikita Novikov <sirpointer@gmail.com>
2022-10-27 14:54:18 +0400

    Setting up

---
+++ Project 13 Instafilter/Project 13 Instafilter/Project 13 Instafilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 13 Instafilter
+//
+//  Created by Nikita Novikov on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
