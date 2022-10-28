decfb5b1949cb850ce1d98e018de8257689f5489
Nikita Novikov <sirpointer@gmail.com>
2022-10-28 11:52:30 +0400

    Preparing for action

--- Project 15 Animation/Project 15 Animation/Project 15 Animation/ViewController.swift
+++ Project 15 Animation/Project 15 Animation/Project 15 Animation/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        view.addSubview(imageView )
     }
 
 
+    @IBAction func Tap(_ sender: UIButton) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 }
 
