a0194c88d786a2b12b357e7b0d3763c7f88554b1
Nikita Novikov <sirpointer@gmail.com>
2022-10-28 11:39:57 +0400

    Setting up

---
+++ Project 15 Animation/Project 15 Animation/Project 15 Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 15 Animation
+//
+//  Created by Nikita Novikov on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
