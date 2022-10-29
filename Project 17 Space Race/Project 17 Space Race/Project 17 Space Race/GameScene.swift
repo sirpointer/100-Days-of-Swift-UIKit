1f06edb7aee36fb8d636d839829a8a1411ccfd2a
Nikita Novikov <sirpointer@gmail.com>
2022-10-29 21:53:12 +0400

    Setting up

---
+++ Project 17 Space Race/Project 17 Space Race/Project 17 Space Race/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project 17 Space Race
+//
+//  Created by Nikita Novikov on 29.10.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
