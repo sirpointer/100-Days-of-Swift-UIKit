415dad978a39fe885525860ef437b37a6850ea37
Nikita Novikov <sirpointer@gmail.com>
2022-10-30 01:42:50 +0400

    Space: the final frontier

--- Project 17 Space Race/Project 17 Space Race/Project 17 Space Race/GameScene.swift
+++ Project 17 Space Race/Project 17 Space Race/Project 17 Space Race/GameScene.swift
@@ -7,10 +7,44 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starField: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        backgroundColor = .black
         
+        print(frame)
+        print(frame.height)
+        print(frame.width)
+        print(frame.width / 2)
+        starField = SKEmitterNode(fileNamed: "starfield")!
+        starField.position = CGPoint(x: frame.width, y: frame.height / 2)
+        starField.advanceSimulationTime(10)
+        starField.zPosition = -1
+        addChild(starField)
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: frame.width * 0.1, y: frame.height / 2)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        score = 0
+        addChild(scoreLabel)
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
     }
     
     override func update(_ currentTime: TimeInterval) {
