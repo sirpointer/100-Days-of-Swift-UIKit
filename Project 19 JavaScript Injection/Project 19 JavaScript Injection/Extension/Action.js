34be39684260aa1e5a41db7eae5a2c827c6f77e8
Nikita Novikov <sirpointer@gmail.com>
2022-10-31 15:06:44 +0400

    settings up js part

---
+++ Project 19 JavaScript Injection/Project 19 JavaScript Injection/Extension/Action.js
@@ -0,0 +1,16 @@
+
+var Action = function() { }
+
+Action.prototipe = {
+    
+run: function(parameters) {
+    
+},
+    
+finalize: function(parameters) {
+    
+}
+    
+};
+
+var ExtensionPreprocessingJS = new Action
