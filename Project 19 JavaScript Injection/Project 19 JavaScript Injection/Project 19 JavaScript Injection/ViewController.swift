7a72ce1e4db296892031cdff5647c57f5bc49582
Nikita Novikov <sirpointer@gmail.com>
2022-10-31 13:12:43 +0400

    Setting up

---
+++ Project 19 JavaScript Injection/Project 19 JavaScript Injection/Project 19 JavaScript Injection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 19 JavaScript Injection
+//
+//  Created by Nikita Novikov on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
