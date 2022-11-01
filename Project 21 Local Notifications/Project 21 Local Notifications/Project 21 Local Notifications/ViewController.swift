5f41f5d3ef9d7b00a9d888938081d1d9d964609e
Nikita Novikov <sirpointer@gmail.com>
2022-11-02 01:01:24 +0400

    Update the code in didReceive so that it shows different instances of UIAlertController depending on which action identifier was passed in.

--- Project 21 Local Notifications/Project 21 Local Notifications/Project 21 Local Notifications/ViewController.swift
+++ Project 21 Local Notifications/Project 21 Local Notifications/Project 21 Local Notifications/ViewController.swift
@@ -48,7 +48,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         dateComponents.minute = 30
 //        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
@@ -68,14 +68,25 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         if let customData = userInfo["customData"] as? String {
             print("Custom data received: \(customData)")
             
+            let title: String?
+            
             switch response.actionIdentifier {
                 case UNNotificationDefaultActionIdentifier:
+                    title = "Default identifier"
                     // user swiped to unlock
                     print("Default identifier")
                 case "show":
-                    print("show more information")
+                    title = "Show more information"
+                    print("Show more information")
                 default:
+                    title = nil
                     break
+            }
+            
+            if let title = title {
+                let ac = UIAlertController(title: title, message: "App was opened from notification", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Ok", style: .default))
+                present(ac, animated: true)
             }
         }
         
