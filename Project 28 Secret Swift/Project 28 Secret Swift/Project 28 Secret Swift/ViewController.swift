550d43dc7b8017c6c0404940a6092fc68bb5b2a2
Nikita Novikov <sirpointer@gmail.com>
2022-11-04 21:54:43 +0400

    Setting up

---
+++ Project 28 Secret Swift/Project 28 Secret Swift/Project 28 Secret Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 28 Secret Swift
+//
+//  Created by Nikita Novikov on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
