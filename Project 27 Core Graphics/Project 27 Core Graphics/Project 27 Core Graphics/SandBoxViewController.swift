cbf628c3f86df29ce3f70cfc430abc56f996513e
Nikita Novikov <sirpointer@gmail.com>
2022-11-07 16:17:01 +0400

    Ellipses and checkerboards

--- Project 27 Core Graphics/Project 27 Core Graphics/Project 27 Core Graphics/SandBoxViewController.swift
+++ Project 27 Core Graphics/Project 27 Core Graphics/Project 27 Core Graphics/SandBoxViewController.swift
@@ -29,6 +29,10 @@ class SandBoxViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
         default:
             break
         }
@@ -51,5 +55,41 @@ class SandBoxViewController: UIViewController {
         
         imageView.image = image
     }
+    
+    func drawCircle() {
+        let side = 512
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: side, height: side))
+        
+        let image = renderer.image { context in
+            let rectangle = CGRect(x: 0, y: 0, width: side, height: side).insetBy(dx: 5, dy: 5)
+            
+            context.cgContext.setFillColor(UIColor.red.cgColor)
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.setLineWidth(10)
+            
+            context.cgContext.addEllipse(in: rectangle)
+            context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCheckerboard() {
+        let side = 512
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: side, height: side))
+        
+        let image = renderer.image { context in
+            context.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0..<8 {
+                for col in 0..<8 {
+                    if (row + col).isMultiple(of: 2) {
+                        context.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        
+        imageView.image = image
+    }
 }
-
