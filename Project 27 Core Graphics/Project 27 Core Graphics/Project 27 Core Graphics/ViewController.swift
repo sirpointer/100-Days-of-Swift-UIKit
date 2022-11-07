d41702fcbdddc666b240ffc8f561b748fea50054
Nikita Novikov <sirpointer@gmail.com>
2022-11-07 13:02:26 +0400

    Setting up

---
+++ Project 27 Core Graphics/Project 27 Core Graphics/Project 27 Core Graphics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 27 Core Graphics
+//
+//  Created by Nikita Novikov on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
