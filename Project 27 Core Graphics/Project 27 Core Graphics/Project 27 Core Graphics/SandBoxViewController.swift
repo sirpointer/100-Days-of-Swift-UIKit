bbe1e36fcb5182779d705aa90c528098967a5a6d
Nikita Novikov <sirpointer@gmail.com>
2022-11-07 16:55:40 +0400

    Images and text

--- Project 27 Core Graphics/Project 27 Core Graphics/Project 27 Core Graphics/SandBoxViewController.swift
+++ Project 27 Core Graphics/Project 27 Core Graphics/Project 27 Core Graphics/SandBoxViewController.swift
@@ -37,6 +37,8 @@ class SandBoxViewController: UIViewController {
             drawRotatedSquares()
         case 4:
             drawLines()
+        case 5:
+            drawImagesAndText()
         default:
             break
         }
@@ -144,6 +146,31 @@ class SandBoxViewController: UIViewController {
             
             context.cgContext.setStrokeColor(UIColor.black.cgColor)
             context.cgContext.strokePath()
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawImagesAndText() {
+        let side = 512
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: side, height: side))
+        
+        let image = renderer.image { context in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 36),
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+            
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            attributedString.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
+            
+            let mouse = UIImage(named: "mouse")
+            mouse?.draw(at: CGPoint(x: 300, y: 150))
         }
         
         imageView.image = image
