8d79b7ef202981483f60b7e5470c396cf30bac13
Nikita Novikov <sirpointer@gmail.com>
2022-11-17 00:45:54 +0400

    Setting up

---
+++ Project 20 Fireworks Night/Project 20 Fireworks Night/Project 20 Fireworks Night/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project 20 Fireworks Night
+//
+//  Created by Nikita Novikov on 17.11.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
